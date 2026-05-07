431c5e8b6bb2cbbe74f573f8ca01f86b2c6387d8
SirBlobFish851415 <twilight.god.mi7@gmail.com>
2026-05-07 17:16:14 +0000

    added front page & refined the main page

---
+++ front.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById("popup").classList.remove("hidden");
+};
+
+document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("popup").classList.add("hidden");
+});
--- script.js
+++ script.js
@@ -17,3 +17,44 @@ document.querySelectorAll(".interactive").forEach(item => {
 popup.addEventListener("click", () => {
     popup.classList.add("hidden");
 });
+
+function placeIconsRandomly() {
+    const icons = document.querySelectorAll('.icon');
+
+    const rows = 3;
+    const cols = 8;
+    const safeZoneHeight = 200;
+    const iconSize = 125;
+
+    const sideSafeZone = 100;
+
+    const slots = [];
+
+    const gridWidth = cols * iconSize;
+
+    const availableWidth = window.innerWidth - sideSafeZone * 2;
+
+    const centerOffset = sideSafeZone + (availableWidth - gridWidth) / 2;
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            const x = centerOffset + c * iconSize;
+            const y = safeZoneHeight + r * iconSize;
+            slots.push({ x, y });
+        }
+    }
+
+    for (let i = slots.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [slots[i], slots[j]] = [slots[j], slots[i]];
+    }
+
+    icons.forEach((icon, index) => {
+        if (slots[index]) {
+            icon.style.left = slots[index].x + "px";
+            icon.style.top = slots[index].y + "px";
+        }
+    });
+}
+
+window.onload = placeIconsRandomly;
